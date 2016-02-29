665da162193c7ec919643bc168ec19d888ea0306
ljg2015 <laurenjgallegos@gmail.com>
2016-02-29 14:54:10 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import bottle
-import random
+
 
 APP = bottle.default_app()
 
@@ -12,7 +12,9 @@ def practice():
   bottle.response.content_type = 'text/html'
   return bottle.static_file('index.html', '.')
    
-  
+ @APP.route('/greet/<salutation>/<name>')
+ def greet(salutation, name):
+   return 'Hellp %s %s' % (salutation, name)
 
 if __name__ == '__main__':
   bottle.run(application=APP)
